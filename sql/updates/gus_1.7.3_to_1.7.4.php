b6396c210541fca0804f954228fbc00267f21f6d
Tom Homer <tomhomer@gmail.com>
2017-03-06 07:30:34 -0500

    Removed hardcoded table names

--- sql/updates/gus_1.7.3_to_1.7.4.php
+++ sql/updates/gus_1.7.3_to_1.7.4.php
@@ -1,7 +1,7 @@
 <?php
 
 // Reduce column size to support UTF8mb4 index size max (which is 191 characters)
-$_SQL[] = "ALTER TABLE `gl_gus_userstats` CHANGE `referer` `referer` VARCHAR(191) NULL DEFAULT NULL;";
-$_SQL[] = "ALTER TABLE `gus_ignore_page` CHANGE `page` `page` VARCHAR(191) NULL DEFAULT '';";
+$_SQL[] = "ALTER TABLE {$_TABLES['gus_userstats']} CHANGE `referer` `referer` VARCHAR(191) NULL DEFAULT NULL;";
+$_SQL[] = "ALTER TABLE {$_TABLES['gus_ignore_page']} CHANGE `page` `page` VARCHAR(191) NULL DEFAULT '';";
 
 ?>
